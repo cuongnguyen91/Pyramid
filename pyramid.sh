265249e55efa2b28dbf5dcd585d7626d2745652c
Cuong Nguyen <nguyencaocuong3333@gmail.com>
2017-05-02 21:47:09 +0700

    for pyramid 5

--- pyramid.sh
+++ pyramid.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 a=1
-for i in {1..9}
+for i in {1..5}
 do
 	a=1
 	while [ "$a" -le "$i" ]
 	do
-		if [ "$a" -le "$i" ]
-		then
-			echo -n "*"
-			a=$(($a + 1))
-		fi
+		echo -n "*"
+		a=$(($a + 1))
 	done
 	echo ""
 done
+for i in {5..1}
+do
+  	a=$(($i-1))
+	while [ "$a" -ge 0 ]
+        do
+		echo -n "*"
+	        a=$(($a - 1))
+        done
+	echo ""
+done
+
